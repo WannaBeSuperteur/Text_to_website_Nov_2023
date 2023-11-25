8e2bfe111b9f9475500a54ef78a077236bb9b765
wannabesuperteur <iwant_optimi@naver.com>
2023-11-25 23:45:26 +0900

    feat: dev-001 text to website HTML code (1)

--- text_to_website.py
+++ text_to_website.py
@@ -3,8 +3,18 @@
 import openai
 
 f = open('api_key.txt', 'r')
-openai.api_key = f.readlines()[0] # OpenAI API KEY
+apikey = f.readlines()[0] # OpenAI API KEY
+openai.api_key = apikey
 f.close()
+
+import os
+import base64
+import requests
+
+header = {
+    'Content-Type': 'application/json',
+    'Authorization': f'Bearer {apikey}'
+}
 
 
 # 입력 텍스트를 5장의 이미지로 변환하는 함수
@@ -30,9 +40,47 @@ def convert_text_to_image(input_text):
 
 
 # 이미지를 읽어서 웹사이트로 변환하는 함수
+# gpt-4-vision-preview API로 이미지에 대한 HTML 코드를 요청해서 결과 읽기
 def convert_image_to_website(image_urls):
+
+    prompt = 'Write an HTML code for the website with the wireframe image.'
+    openai_api_url = 'https://api.openai.com/v1/chat/completions'
+
+    # OpenAI API로 요청하는 함수
+    def request_to_openai_api(url):
+        payload = {
+            'model': 'gpt-4-vision-preview',
+            'messages': [
+                {
+                    'role': 'user',
+                    'content': [
+                        {
+                            'type': 'text',
+                            'text': prompt
+                        },
+                        {
+                            'type': 'image_url',
+                            'image_url': {'url': url}
+                        }
+                    ]
+                }
+            ],
+            'max_tokens': 3585
+        }
+
+        response = requests.post(url=openai_api_url, headers=header, json=payload)
+        print(f'\nresponse:\n{response.json()}')
+
+        return response.json()
+
+    # 각 URL마다 OpenAI API (gpt-4-vision-preview) 로 요청한 HTML 코드 받기
+    resps = []
+    
     for url in image_urls:
-        pass
+        resp = request_to_openai_api(url)
+        resps.append(resp)
+
+    return resps
 
 
 # 입력 텍스트를 이미지로 바꾸고, 그 이미지를 웹사이트로 변환
@@ -42,3 +90,4 @@ def convert_text_to_website(input_text):
     image_urls = convert_text_to_image(input_text)
 
     # 이미지 -> 웹사이트 변환
+    website_html_codes = convert_image_to_website(image_urls)

90e44348c5af53f94c792db72c26ebaea75bbe70
wannabesuperteur <iwant_optimi@naver.com>
2023-11-25 22:28:37 +0900

    feat: dev-001 add text to image using DALL-E

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import text_to_website as t2w
 
 if __name__ == '__main__':
+
+    # 텍스트 입력
     print('input text for creating website :')
     input_text = input()
+
+    # 텍스트를 이미지로, 이미지를 웹사이트로 변환
     t2w.convert_to_website(input_text)
--- text_to_website.py
+++ text_to_website.py
@@ -1,4 +1,38 @@
 # convert input text to website
 
+import openai
+
+f = open('api_key.txt', 'r')
+openai.api_key = f.readlines()[0] # OpenAI API KEY
+f.close()
+
+
+# 입력 텍스트를 5장의 이미지로 변환하는 함수
+def convert_to_image(input_text):
+
+    num = 5 # 이미지 개수 (1분당 5장 초과의 경우 오류 발생)
+
+    # DALL-E API를 통해 이미지를 요청하는 부분
+    response = openai.Image.create(
+        prompt = f'a website with {input_text}',
+        n = num,
+        size = '1024x1024'
+    )
+
+    # 생성된 이미지의 링크 목록
+    image_urls = [response['data'][x]['url'] for x in range(num)]
+
+    # 생성된 이미지의 링크 출력 및 반환
+    for i in range(num):
+        print(f'\nresult image url:\n{image_urls[i]}')
+
+    return image_urls
+
+
+# 입력 텍스트를 이미지로 바꾸고, 그 이미지를 웹사이트로 변환
 def convert_to_website(input_text):
-    pass
+
+    # 텍스트 -> 이미지 변환
+    image_urls = convert_to_image(input_text)
+
+    # 이미지 -> 웹사이트 변환
